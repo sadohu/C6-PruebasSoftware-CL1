a02c00862d6bcc5fd36a41790513c4d332a3d050
sadohu <sadohu26@yahoo.com>
2023-11-17 14:15:30 -0500

    Add essential test

---
+++ src/main/java/pe/edu/cibertec/Hotel.java
@@ -0,0 +1,82 @@
+package pe.edu.cibertec;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Hotel {
+    private int piso;
+    private int habitacion;
+    private String nombreCliente;
+    private LocalDate fechaReserva;
+
+    public Hotel() {
+    }
+
+    public Hotel(int piso, int habitacion, String nombreCliente, LocalDate fechaReserva) {
+        this.piso = piso;
+        this.habitacion = habitacion;
+        this.nombreCliente = nombreCliente;
+        this.fechaReserva = fechaReserva;
+    }
+
+    public String registrarReserva(String numberoHabitacion, String nombreCliente, String fechaReserva){
+        boolean invalid;
+
+        // Validación de números
+        invalid = !numberoHabitacion.matches("[\\d+]{3}");
+        if(invalid)
+            return "Ingrese una habitación válida (Números enteros y máximo tres caracteres).";
+
+        // Validar número de piso
+        int piso = Integer.parseInt(numberoHabitacion.substring(0, 1));
+        invalid = (piso < 1) || (piso > 3);
+        if(invalid)
+            return "Ingrese un número de piso válido, del 1 al 3.";
+
+
+        // CASO 1: Asegurar eliminación de los espacios excesivos
+        nombreCliente = nombreCliente.trim();
+        nombreCliente = nombreCliente.replaceAll("\\s+", " ");
+
+        // Validar cantidad de caracteres
+        invalid = nombreCliente.length() < 4;
+        if(invalid)
+            return "Recuerde que el nombre del cliente debe contener al menos 4 caracteres.";
+
+        // CASO 2: Controlar los espacios mediante una advertencia al usuario.
+        /*
+            invalid = !nombre.matches("[a-zA-Zá-úÁ-Ú]+(\\s[a-zA-Zá-úÁ-Ú]+)*");
+            assertTrue(valid, "Recuerde que el nombre del cliente debe contener al menos 4 caracteres y contener caracteres alfabéticos.\nPor último, sólo se admiten un espacio entre nombres.");
+        */
+
+        invalid = !nombreCliente.matches("[a-zA-Zá-úÁ-Ú ]+");
+        if(invalid)
+            return "Recuerde que el nombre del cliente debe contener al menos 4 caracteres y contener caracteres alfabéticos.";
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fecha = LocalDate.of(1, 1 ,1);
+
+        // Controlar si se ingresa una fecha válida
+        try {
+            fecha = LocalDate.parse(fechaReserva, formatter);
+        } catch (DateTimeParseException e) {
+            return "Ingrese un formato de Días/Meses/Año, Ej 01/12/2025";
+        }
+
+        LocalDate systemDate = LocalDate.now();
+        invalid = !fecha.isAfter(systemDate);
+        if(invalid)
+            return "Ingrese una fecha superior a la actual.";
+
+
+        System.out.println(
+            "Resúmen de reserva: \n" +
+            "Nombre del cliente: " + this.nombreCliente + "\n" +
+            "Habitación: " + String.valueOf(this.piso) + String.valueOf(this.habitacion) + "\n" +
+            "Fecha de reserva: " + this.fechaReserva);
+
+        return "El registro ha sido exitoso.";
+    }
+}
---
+++ src/test/java/pe/edu/cibertec/HotelTest.java
@@ -0,0 +1,65 @@
+package pe.edu.cibertec;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HotelTest {
+
+    @Test
+    public void pruebaRegistroExitoso() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("101", "Juan Pérez", "22/12/2023");
+        assertEquals("El registro ha sido exitoso.", resultado);
+    }
+
+    @Test
+    public void pruebaNumeroHabitacionInvalido() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("A01", "Juan Pérez", "22/12/2023");
+        assertEquals("Ingrese una habitación válida (Números enteros y máximo tres caracteres).", resultado);
+    }
+
+    @Test
+    public void pruebaNumeroPisoInvalido() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("401", "Juan Pérez", "22/12/2023");
+        assertEquals("Ingrese un número de piso válido, del 1 al 3.", resultado);
+    }
+
+    @Test
+    public void pruebaNombreClienteInvalido() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("101", "J1", "22/12/2023");
+        assertEquals("Recuerde que el nombre del cliente debe contener al menos 4 caracteres.", resultado);
+    }
+
+    @Test
+    public void pruebaNombreClienteCaracteresInvalidos() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("101", "Juan123", "22/12/2023");
+        assertEquals("Recuerde que el nombre del cliente debe contener al menos 4 caracteres y contener caracteres alfabéticos.", resultado);
+    }
+
+    @Test
+    public void pruebaEspaciosNombreCliente() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("101", "   J   P   ", "22/12/2023");
+        assertEquals("Recuerde que el nombre del cliente debe contener al menos 4 caracteres.", resultado);
+    }
+
+    @Test
+    public void pruebaFechaFormatoInvalido() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("101", "Juan Pérez", "2023/12/10");
+        assertEquals("Ingrese un formato de Días/Meses/Año, Ej 01/12/2025", resultado);
+    }
+
+    @Test
+    public void pruebaFechaReservaInvalida() {
+        Hotel hotel = new Hotel();
+        String resultado = hotel.registrarReserva("101", "Juan Pérez", "22/12/2020");
+        assertEquals("Ingrese una fecha superior a la actual.", resultado);
+    }
+
+}
